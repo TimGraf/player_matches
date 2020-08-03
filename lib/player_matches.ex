28e2225cec0b6bc77bf9296adc8d0a2b5a8c0bdb
Timothy Graf <polyglotdev@protonmail.com>
2020-08-03 11:54:14 -0700

    monitor players

--- lib/player_matches.ex
+++ lib/player_matches.ex
@@ -46,7 +46,9 @@ defmodule PlayerMatches do
     @doc """
     """
     def monitor_players_in_matches(region, matches) do
-        Logger.info("PlayerMatches: monitoring players in matches ...")
+        Logger.info("PlayerMatches: monitoring players in last 5 matches ...")
+
+        last_game = Enum.at(matches, 0)
 
         player_set = MapSet.new(List.flatten(Enum.map(matches, fn match -> 
             case SummonerClient.get_match_details(region, match.gameId) do
@@ -55,10 +57,42 @@ defmodule PlayerMatches do
             end
         end)))
 
-        Logger.info("#{MapSet.size(player_set)} Players")
+        {:ok, date_time} = DateTime.from_unix(last_game.timestamp, :millisecond)
 
-        Enum.map(player_set, fn player ->
-            Logger.info("Player Name: #{player.summonerName}")
+        Logger.info("Logging new games for players since: #{DateTime.to_string(date_time)}")
+
+        poll(region, player_set, last_game.timestamp, 0)
+    end
+
+    @doc """
+    """
+    def poll(region, players, log_since, count) do
+        if count <= 5 do
+            receive do
+            after
+            60_000 ->
+                log_matches(region, players, log_since)
+                poll(region, players, log_since, count + 1)
+            end
+        end
+    end
+
+    @doc """
+    """
+    def log_matches(region, players, log_since) do
+        Enum.map(players, fn player ->
+            case SummonerClient.get_last_n_matches(region, player.accountId, 5) do
+                {:ok, match_list} -> 
+                    Enum.map(match_list.matches, fn match ->
+                        if match.timestamp > log_since do
+                            player_name = player.summonerName
+                            match_id = match.gameId
+                            {:ok, date_time} = DateTime.from_unix(match.timestamp, :millisecond)
+                            Logger.info("New Match for Player: #{player_name}, Match ID: #{match_id}, At: #{DateTime.to_string(date_time)}")
+                        end
+                    end)
+                {:error, _error} -> Logger.error("Error getting matches.")
+            end
         end)
     end
 end
